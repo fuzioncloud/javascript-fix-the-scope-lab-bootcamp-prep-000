506d4fd0ae6861656b63fff3636250f7d116abc9
fuzioncloud <zzaharris@fuzioncloud.com>
2017-10-03 19:15:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ var funkyFunction = function() {
     return "FUNKY!"
   }
 }
-console.log(funkyFunction)
-console.log(funkyFunction())
+// console.log(funkyFunction)
+// console.log(funkyFunction())
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction()
 console.log(theFunk())
-
-console.log(theFunk)
-console.log(funkyFunction(function(){}))
+// 
+// console.log(theFunk)
+// console.log(funkyFunction(function(){}))
