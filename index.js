72674d112b7bdac302d1f9c990812e9e8f8865ef
fuzioncloud <zzaharris@fuzioncloud.com>
2017-10-03 19:17:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ var funkyFunction = function() {
 var theFunk = function () {
   return funkyFunction()
 }
-console.log(theFunk)
+console.log(theFunk())
 //
 // console.log(theFunk)
 // console.log(funkyFunction(function(){}))
